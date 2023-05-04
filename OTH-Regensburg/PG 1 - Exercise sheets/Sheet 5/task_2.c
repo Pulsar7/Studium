fb00ec9b59b772867d9f537bd250502acf15d585
Pulsar7 <pythonpulsar@gmail.com>
2023-05-04 15:06:00 +0200

    completed task_2 of Sheet 5

--- OTH-Regensburg/PG 1 - Exercise sheets/Sheet 5/task_2.c
+++ OTH-Regensburg/PG 1 - Exercise sheets/Sheet 5/task_2.c
@@ -1,51 +1,47 @@
 #include <stdio.h>
 
 
-int get_integer_input() {
-    int num = 0, ch;
-    do {
-        ch = getchar();
-        if (ch >= 48 && ch <= 57) {
-            num = (num * 10 + (ch - 48)); // convert to digit
-        } else if ((int)ch != 10) { // checks if ASCII-Code of character is not ' ' (found deviant!)
-            num = -1;
-            break;
-        }
-        if (ch == '\n') {
-            break;
-        }
-    } while (1);
-    return num;
-}
-
-int invalid_input(int *zahl) {
-    printf("Die Zahl '%d' wird nicht akzeptiert!\n",*zahl);
-}
-
 int main(int argc, char* argv[]) {
     int *numbers[100] = {0};
     char *operators[] = {
         '+','-','/','*','%'
     };
-    int quit = 0, number;
-    float result;
+    int number;
+    double result;
     char operator;
-    while (quit == 0) {
-        printf("Geben Sie eine Zahl ein:\n");
-        number = get_integer_input();
-        if (number != -1) {
-            printf("Geben Sie einen Operator ein:\n");
-            scanf("%c",&operator);
-            if (operator == 'q') {
-                quit = 1;
-            } else {
-                
+    printf("Geben Sie eine Zahl ein:\n");
+    scanf(" %d",&result);
+    while (1) {
+        printf("Geben Sie einen Operator ein:\n");
+        scanf(" %c",&operator);
+        if (operator != 'q') {
+            printf("Geben Sie eine Zahl ein:\n");
+            scanf(" %d",&number);
+            switch (operator) {
+                case '+':
+                    result = result+number;
+                    break;
+                case '-':
+                    result = result-number;
+                    break;
+                case '/':
+                    if (number != 0) {
+                        result = result/number;
+                    } else {
+                        printf("Cannot divide with zero!\n");
+                    }
+                    break;
+                case '*':
+                    result = result*number;
+                    break;
+                case '%':
+                    result = (int)result%number;
+                    break;
             }
+            printf(" = %f\n",result);
         } else {
-            invalid_input(&number);
-            quit = 1;
+            break;
         }
     }
-    printf("Result = %f\n");
     return 0;
 }

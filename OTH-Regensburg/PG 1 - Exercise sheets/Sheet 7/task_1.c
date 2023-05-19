b5b1c8e1d1ff3e6e715f42ff1e35a9f85ecdd1d7
Pulsar7 <pythonpulsar@gmail.com>
2023-05-19 12:44:06 +0200

    added inefficient sort-algorithm for integer-array.

--- OTH-Regensburg/PG 1 - Exercise sheets/Sheet 7/task_1.c
+++ OTH-Regensburg/PG 1 - Exercise sheets/Sheet 7/task_1.c
@@ -19,6 +19,36 @@ int get_integer(void) {
     return num;
 }
 
+int sort_array(int *array, int length) { // inefficient
+    int counter = 0,current,next,not_changed = 0;
+    while (not_changed < length) {
+        if (counter == 0) {
+            not_changed = 0;
+        }
+        if ((counter+1) <= length-1) {
+            if (array[counter] > array[counter+1]) {
+                current = array[counter];
+                next = array[counter+1];
+                array[counter] = next;
+                array[counter+1] = current;
+            } else {
+                not_changed++;
+            }
+        } else {
+            not_changed++;
+        }
+        counter++;
+        if (counter > length) {
+            counter = 0;
+        }
+    }
+    for (int i = 0; i < length; i++) {
+        printf("%d,",array[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
 
 int main(int argc, const char **argv) {
     int my_numbers[MAX_INTEGERS],counter = 0,number;
@@ -27,7 +57,7 @@ int main(int argc, const char **argv) {
         if (number != -1) {
             my_numbers[counter] = number;
         } else {
-            printf("Input error!\n");
+            // printf("Input error!\n");
             break;
         }
         counter++;
@@ -38,7 +68,6 @@ int main(int argc, const char **argv) {
         if (i == 0) {
             minimum = my_numbers[i];
         }
-        printf("(%d) %d\n",i,my_numbers[i]);
         sum += my_numbers[i];
         if (my_numbers[i] > maximum) {
             maximum = my_numbers[i];
@@ -50,5 +79,11 @@ int main(int argc, const char **argv) {
     printf("Minium: %d\n",minimum);
     printf("Maximum: %d\n",maximum);
     printf("Average: %f\n",average);
+    printf("Unsorted Integer-Array:\n");
+    for (int i = 0; i < counter; i++) {
+        printf("%d,",my_numbers[i]);
+    }
+    printf("\nSorted Integer-Array:\n");
+    sort_array(my_numbers,counter); // return obj
     return 0;
 }

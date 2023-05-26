d33670aacb0b2f092036683d4e447751606ee8de
Pulsar7 <pythonpulsar@gmail.com>
2023-05-26 19:55:12 +0200

    Completed task_2.

--- OTH-Regensburg/PG 1 - Exercise sheets/Sheet 8/task_2.c
+++ OTH-Regensburg/PG 1 - Exercise sheets/Sheet 8/task_2.c
@@ -2,16 +2,14 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_NUMBER 100000000
-#define MIN_NUMBER 0
 #define MAX_USER_INPUT_LEN 10
 
 
 typedef enum {True=0,False=1} BOOLEAN;
 
 
-int pseudo_random_number(void) {
-    return ((rand()%MAX_NUMBER)+MIN_NUMBER);
+int pseudo_random_number(int max_number, int min_number) {
+    return ((rand()%max_number)+min_number);
 }
 
 BOOLEAN validate_integer(char number_element) {
@@ -56,7 +54,12 @@ int main(int argc, char **argv) {
     if (status == True) {
         int k = to_number(user_input[0]);
         int n = to_number(user_input[2]);
-        
+        int number;
+        for (int i = 0; i <= k; i++) {
+            number = pseudo_random_number(n,1);
+            result += number;
+        }
+        printf("Result = %d\n",result);
     } else {
         printf("Your input '%s' has been denied! It's invalid.\n",user_input);
     }

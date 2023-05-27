f0025e8e444dab79b205c04e01219bba753adf1d
Pulsar7 <pythonpulsar@gmail.com>
2023-05-27 12:03:57 +0200

    modified:   Eigene Aufgaben - Projekte/Vector-Msg-Encryption/vector_msg_encryption.cpp

--- Eigene Aufgaben - Projekte/Vector-Msg-Encryption/vector_msg_encryption.cpp
+++ Eigene Aufgaben - Projekte/Vector-Msg-Encryption/vector_msg_encryption.cpp
@@ -1,7 +1,14 @@
+/*
+    New version of the "A.M.M.E. - Encryption"
+*/
 #include <iostream>
 #include <time.h>
+#include <stdio.h>
 #include <string>
+#include <stdio.h>
 using namespace std;
+#define MAX_NUMBER 10000000
+#define MIN_NUMBER 0
 
 
 class COLOR_PRINT {
@@ -35,15 +42,26 @@ class ENCRYPTION:COLOR_PRINT {
     public:
         int key_length;
         string key;
+        char all_characters[52] = {
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        };
+        int vectors[52][2];
 
         bool generate_key(void) {
+            
+        }
 
+    private:
+        int pseudo_random_number(void) {
+            return ((rand()%MAX_NUMBER)+MIN_NUMBER);
         }
 
 };
 
 
 int main(int argc, const char **argv) {
+    srand(time(NULL));
     ENCRYPTION encryption;
     COLOR_PRINT cp;
     encryption.key_length = 100;

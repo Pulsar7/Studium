68b1914d37b126723cb7172a05ba048542f7b986
Pulsar7 <pythonpulsar@gmail.com>
2023-06-23 13:05:16 +0200

    new file:   OTH-Regensburg/MA 1/Matrix Calculations/matrix_calculator.c 	new file:   OTH-Regensburg/MA 1/Matrix Calculations/matrix_multiplication

---
+++ OTH-Regensburg/MA 1/Matrix Calculations/matrix_calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int multiply(int *matrix) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            
+        }
+    }
+}
+
+int main(int argc, const char **argv) {
+    int matrix[2][3] = {
+        {1,2,3},
+        {5,6,7},
+        {8,9,10}
+    };
+    multiply(matrix);
+    return 0;
+}

a7fe5fea8ba2340fa94aa0c86c2c0ae4da378179
Pulsar7 <pythonpulsar@gmail.com>
2023-07-02 23:12:40 +0200

    modified:   OTH-Regensburg/MA 1/Matrix Calculations/matrix_calculator 	modified:   OTH-Regensburg/MA 1/Matrix Calculations/matrix_calculator.c 	deleted:    OTH-Regensburg/MA 1/Matrix Calculations/matrix_multiplication

--- OTH-Regensburg/MA 1/Matrix Calculations/matrix_calculator.c
+++ OTH-Regensburg/MA 1/Matrix Calculations/matrix_calculator.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define END_MARKER -99999
 
-
-int get_rows(int **matrix) {
-    int rows = 0;
-    while (rows < 200) {
-        if (matrix[rows][0] == END_MARKER) {
-            break;
-        }
-        rows++;
-    }
-    rows++;
-    return rows;
-}
-
-int get_cols(int **matrix) {
-    int cols = 0;
-    while (cols < 200) {
-        if (matrix[0][cols] == END_MARKER) {
-            break;
-        }
-        cols++;
-    }
-    cols++;
-    return cols;
-}
 
 int multiply(int **matrixA, int **matrixB, int **result_matrix) {
     
@@ -34,27 +11,28 @@ int skalar_multiply(int **matrixA, int skalar) {
     
 }
 
-int sum(int **matrixA, int **matrixB) {
+int** sum(int **matrixA, int **matrixB, int rows_A, int rows_B) {
     // Only matrices whose number of rows is equal to the number of columns can be added.
-    int rows = get_cols(matrixA), cols = get_cols(matrixA);
-    printf("ROWS=%d | COLS=%d\n",rows,cols);
-    int result_matrix[rows][cols];
+    printf("ROWS-A=%d | ROWS-B=%d\n",rows_A,rows_B);
+    /*int **result_matrix = (int**)malloc(rows_A * sizeof(int*));
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             result_matrix[i][j] = matrixA[i][j]+matrixB[i][j];
         }
     }
-    return result_matrix;
+    return (int**)result_matrix;*/
 }
 
-int show(int **matrix) {
+/*int show(int **matrix) {
     for (int i = 0; i < get_rows(matrix); i++) {
         for (int j = 0; j < get_cols(matrix); j++) {
             printf("%d",matrix[i][j]);
         }
         printf("\n");
     }
-}
+}*/
+
+
 
 int main(int argc, const char **argv) {
     int matrixA[4][4] = {
@@ -69,7 +47,25 @@ int main(int argc, const char **argv) {
         {11,19,8,END_MARKER},
         {END_MARKER,END_MARKER,END_MARKER,END_MARKER}
     };
-    int **result_matrix = sum(matrixA,matrixB);
-    show(result_matrix);
+
+    int rows_A = 0, rows_B = 0;
+    while (rows_A < 200) {
+        if (matrixA[rows_A][0] == END_MARKER) {
+            rows_A++;
+            break;
+        }
+        rows_A++;
+    }
+    while (rows_B < 200) {
+        if (matrixB[rows_B][0] == END_MARKER) {
+            rows_B++;
+            break;
+        }
+        rows_B++;
+    }
+    
+    int **result_matrix = sum((int**)matrixA,(int**)matrixB,rows_A,rows_B);
+    // show(result_matrix);
+    // free(result_matrix); // free allocated space
     return 0;
 }

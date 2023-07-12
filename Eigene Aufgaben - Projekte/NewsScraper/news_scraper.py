3a072ab8bf20d1707b3c97bf2a0b47bba5feae29
Pulsar7 <pythonpulsar@gmail.com>
2023-07-12 12:39:17 +0200

    Updated all files and directories.

---
+++ Eigene Aufgaben - Projekte/NewsScraper/news_scraper.py
@@ -0,0 +1,12 @@
+import os,time,sys,requests
+
+
+class SCRAPER():
+    def __init__(self) -> None:
+        pass
+    
+    
+if __name__ == '__main__':
+    os.system("clear") # 
+    scraper = SCRAPER()
+    scraper.run()
